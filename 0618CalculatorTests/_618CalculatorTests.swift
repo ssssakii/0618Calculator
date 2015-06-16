282749fa19669e6f6ad995f4737f5f9f7e48c5f1
高田早記 <abare.sy@gmail.com>
2015-06-16 11:28:39 +0900

    Initial Commit

---
+++ 0618CalculatorTests/_618CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  _618CalculatorTests.swift
+//  0618CalculatorTests
+//
+//  Created by 高田早記 on 2015/06/16.
+//  Copyright (c) 2015年 高田早記. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _618CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

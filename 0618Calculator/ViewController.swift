4e49662f42e195e66698db8a51f4d961f67010ac
高田早記 <abare.sy@gmail.com>
2015-06-17 14:54:01 +0900

    second commit

--- 0618Calculator/ViewController.swift
+++ 0618Calculator/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet var label:UILabel!
     
-    var number:Int = 0
-    var number2:Int = 0
-    var operation:Int = 0
+    var number:Double = 0
+    var number2:Double = 0
+    var operation:Double = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,52 +29,52 @@ class ViewController: UIViewController {
     
     @IBAction func select0() {
         number = number*10 + 0
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select1() {
         number = number*10 + 1
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select2() {
         number = number*10 + 2
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select3() {
         number = number*10 + 3
-        label.text = String(number)
+        label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select4() {
         number  = number*10 + 4
-        label.text = String(number)
+        label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select5() {
         number = number*10 + 5
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select6() {
         number = number*10 + 6
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select7() {
         number = number*10 + 7
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select8() {
         number = number*10 + 8
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     @IBAction func select9() {
         number = number*10 + 9
-            label.text = String(number)
+            label.text = String(stringInterpolationSegment: number)
     }
     
     
@@ -89,13 +90,13 @@ class ViewController: UIViewController {
     
     @IBAction func equal() {
         if operation == 1{
-            label.text = String(number + number2)
+            label.text = String(stringInterpolationSegment: number + number2)
         }else if operation == 2 {
-            label.text = String(number2 - number)
+            label.text = String(stringInterpolationSegment:number2 - number)
         }else if operation == 3 {
-            label.text = String(number * number2)
+            label.text = String(stringInterpolationSegment: number * number2)
         }else{
-            label.text = String(number2 / number)
+            label.text = String(stringInterpolationSegment: number2 / number)
         }
     }
     
@@ -122,6 +123,6 @@ class ViewController: UIViewController {
     
     @IBAction func clear(){
         number = number - number
-        label.text = String(number)
+        label.text = String(stringInterpolationSegment: number)
     }
 }
